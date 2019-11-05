0c90995fb94c25838982e2baf3e0eccce02dff30
Jack Meyer <jackcmeyer@gmail.com>
2019-11-04 22:40:39 -0600

    feat(init): initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+    tsconfigRootDir: './',
+    createDefaultProgram: true,
+  },
+  settings: {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.ts', '.tsx']
+      }
+    }
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest',
+  ],
+  rules: {
+    'react/jsx-filename-extension': ['error', { 'extensions': ['.tsx'] }],
+    'no-param-reassign': ['error', { 'props': false }],
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+  },
+};
---
+++ README.md
@@ -0,0 +1,46 @@
+# React Typescript Template
+
+[![Build Status](https://travis-ci.com/jackcmeyer/react-typescript-template.svg?token=iRTqViLzvrxp31hGqdbu&branch=master)](https://travis-ci.com/jackcmeyer/react-typescript-template)
+[![Commitizen friendly](https://img.shields.io/badge/commitizen-friendly-brightgreen.svg)](http://commitizen.github.io/cz-cli/)
+
+## Description
+
+React Typescript Template with React, Typescript, ESLint, TravisCI, Commitizen, Yarn
+
+## Installation
+
+```bash
+$ yarn install
+```
+
+## Running the app
+
+```bash
+# build
+$ yarn build
+
+# development
+$ yarn start
+
+# watch mode
+$ yarn start
+```
+
+## Test
+
+```bash
+# unit tests
+$ yarn test
+
+# lint
+$ yarn lint
+```
+
+## Stay in touch
+
+- [Website](https://jackcmeyer.com)
+- Twitter - [@jackcmeyer](https://twitter.com/jackcmeyer)
+
+## License
+
+React Typescript Template is [MIT licensed](LICENSE).
